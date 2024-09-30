69f300d53a6995f6e7e5d549024eff2576430c18
Manav Mehta <s223608412@deakin.edu.au>
2024-10-01 00:22:14 +1000

    initial commit

--- inventory_management/app/config.py
+++ inventory_management/app/config.py
@@ -1,3 +1,3 @@
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@host:port/dbname')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://sit722_part5_gf8d_user:ATs755Lec85LjpQWogi67yLX38LS1iju@dpg-crsjf5rtq21c73dftu5g-a.oregon-postgres.render.com/sit722_part5_gf8d')

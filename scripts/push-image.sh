d6455c6d48bd8875e54221adf64cd55d3379e108
Manav Mehta <s223608412@deakin.edu.au>
2024-10-01 02:28:19 +1000

    update 2

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -10,5 +10,6 @@ if [ -z "$VERSION" ]; then
   exit 1
 fi
 
+echo $REGISTRY_PW | docker login $CONTAINER_REGISTRY.azurecr.io --username $REGISTRY_UN --password-stdin
 docker push $CONTAINER_REGISTRY.azurecr.io/book_catalog:$VERSION
 docker push $CONTAINER_REGISTRY.azurecr.io/inventory_management:$VERSION
